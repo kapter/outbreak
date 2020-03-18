4ad797209c3e731672372c7df9a186a72c5be068
kapter <pvv@interc.ru>
2020-03-18 11:37:33 +0300

    Updates

--- precache-manifest.6bb2f6609750b29f4b6054b0c3515282.js
+++ precache-manifest.6bb2f6609750b29f4b6054b0c3515282.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "655803234911e0cd6420",
+    "revision": "5e411dacec1a65d07b6f",
     "url": "/outbreak/static/css/main.6f48272a.chunk.css"
   },
   {
-    "revision": "655803234911e0cd6420",
-    "url": "/outbreak/static/js/main.65580323.chunk.js"
+    "revision": "5e411dacec1a65d07b6f",
+    "url": "/outbreak/static/js/main.5e411dac.chunk.js"
   },
   {
     "revision": "5a6546b745159b94c07c",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/outbreak/static/js/runtime~main.c0f49201.js"
   },
   {
-    "revision": "05d90e97843fdcb33dc26ac454bebae4",
+    "revision": "c948a899af8147b4401bcf07802d1d9a",
     "url": "/outbreak/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/outbreak/precache-manifest.89141c266fd44de32c404d122dbafb14.js"
+  "/outbreak/precache-manifest.6bb2f6609750b29f4b6054b0c3515282.js"
 );
 
 workbox.clientsClaim();
