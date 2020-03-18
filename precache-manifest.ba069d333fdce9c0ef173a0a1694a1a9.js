a0bc0c037e44a0278a100d296906f6e2d44a950d
kapter <pvv@interc.ru>
2020-03-18 08:22:01 +0300

    Updates

---
+++ precache-manifest.ba069d333fdce9c0ef173a0a1694a1a9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "eb515409f4716f43ee04",
+    "url": "/outbreak/static/css/main.6f48272a.chunk.css"
+  },
+  {
+    "revision": "eb515409f4716f43ee04",
+    "url": "/outbreak/static/js/main.eb515409.chunk.js"
+  },
+  {
+    "revision": "5a6546b745159b94c07c",
+    "url": "/outbreak/static/js/1.5a6546b7.chunk.js"
+  },
+  {
+    "revision": "c0f4920164c0c8234d85",
+    "url": "/outbreak/static/js/runtime~main.c0f49201.js"
+  },
+  {
+    "revision": "d6e2826005e45081ad87b00df68ee6a1",
+    "url": "/outbreak/index.html"
+  }
+];
